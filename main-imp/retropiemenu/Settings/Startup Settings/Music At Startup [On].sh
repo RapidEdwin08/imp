0436ddd23c6c9fda9eceb6cc341e47111624c8ea
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2021-11-01 21:09:00 -0400

    Add files via upload

---
+++ main-imp/retropiemenu/Settings/Startup Settings/Music At Startup [On].sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+IMP=/opt/retropie/configs/imp
+IMPSettings=$IMP/settings
+echo "1" > $IMPSettings/music-startup.flag
+
+# v2021.11 Addition - Runs at [IMP] install + Runs here in case of [RetroPie-Setup] Scripts gets Updated
+rpsKODIautostart=~/RetroPie-Setup/scriptmodules/supplementary/autostart.sh
+rpsKODI='echo -e \"kodi #auto\\nemulationstation #auto\" >>\"$script\"'
+rpsKODIsa='echo -e \"kodi-standalone #auto\\nemulationstation #auto\" >>\"$script\"'
+rpsKODIes='echo \"emulationstation #auto\" >>\"$script\"'
+impKODI='echo -e \"kodi #auto\\nbash \/opt\/retropie\/configs\/imp\/boot.sh > \/dev\/null 2>\&1 \& #auto\\nemulationstation #auto\" >>\"$script\"'
+impKODIsa='echo -e \"kodi-standalone #auto\\nbash \/opt\/retropie\/configs\/imp\/boot.sh > \/dev\/null 2>\&1 \& #auto\\nemulationstation #auto\" >>\"$script\"'
+impKODIes='echo -e \"bash \/opt\/retropie\/configs\/imp\/boot.sh > \/dev\/null 2>\&1 \& #auto\\nemulationstation #auto\" >>\"$script\"'
+
+# Modifications to Retain [IMP] when Switching ES/Kodi on Boot from RetroPie-Setup
+# --- [Default] [~/RetroPie-Setup/scriptmodules/supplementary/autostart.sh]  ---
+#            echo -e "kodi #auto\nemulationstation #auto" >>"$script"
+#            echo -e "kodi-standalone #auto\nemulationstation #auto" >>"$script"
+#            echo "emulationstation #auto" >>"$script"
+
+# --- [IMP] [~/RetroPie-Setup/scriptmodules/supplementary/autostart.sh]  ---
+#            echo -e "kodi #auto\nbash /opt/retropie/configs/imp/boot.sh > /dev/null 2>&1 & #auto\nemulationstation #auto" >>"$script"
+#            echo -e "kodi-standalone #auto\nbash /opt/retropie/configs/imp/boot.sh > /dev/null 2>&1 & #auto\nemulationstation #auto" >>"$script"
+#            echo -e "bash /opt/retropie/configs/imp/boot.sh > /dev/null 2>&1 & #auto\nemulationstation #auto" >>"$script"
+
+# Add [IMP] to the Auto-start ES/Kodi on Boot Script [~/RetroPie-Setup/scriptmodules/supplementary/autostart.sh]
+sed -i s+"$rpsKODI"+"$impKODI"+ $rpsKODIautostart
+sed -i s+"$rpsKODIsa"+"$impKODIsa"+ $rpsKODIautostart
+sed -i s+"$rpsKODIes"+"$impKODIes"+ $rpsKODIautostart
+
+tput reset
+exit 0
---
+++ main-imp/retropiemenu/Settings/Startup Settings/[070] Delay At Startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+delay_startup=070
+
+IMP=/opt/retropie/configs/imp
+IMPSettings=$IMP/settings
+echo "$delay_startup" > $IMPSettings/delay-startup.flag
+tput reset
+exit 0
