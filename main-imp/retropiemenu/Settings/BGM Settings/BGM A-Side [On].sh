ac83272f9d1ea1955ca95f239462a73100a49a03
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2021-11-01 21:04:42 -0400

    Add files via upload

---
+++ main-imp/retropiemenu/Settings/BGM Settings/BGM A-Side [On].sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+IMP=/opt/retropie/configs/imp
+IMPSettings=$IMP/settings
+echo "1" > $IMPSettings/a-side.flag
+
+# Swap Icon of [Start All Music] to Reflect BGM Settings
+rm ~/RetroPie/retropiemenu/icons/impstartall.png
+
+if [ $(cat $IMPSettings/a-side.flag) == "0" ] && [ $(cat $IMPSettings/b-side.flag) == "0" ]; then
+	cp ~/RetroPie/retropiemenu/icons/impstartalla0b0.png ~/RetroPie/retropiemenu/icons/impstartall.png
+fi
+
+if [ $(cat $IMPSettings/a-side.flag) == "1" ] && [ $(cat $IMPSettings/b-side.flag) == "0" ]; then
+	cp ~/RetroPie/retropiemenu/icons/impstartalla1b0.png ~/RetroPie/retropiemenu/icons/impstartall.png
+fi
+
+if [ $(cat $IMPSettings/a-side.flag) == "0" ] && [ $(cat $IMPSettings/b-side.flag) == "1" ]; then
+	cp ~/RetroPie/retropiemenu/icons/impstartalla0b1.png ~/RetroPie/retropiemenu/icons/impstartall.png
+fi
+
+if [ $(cat $IMPSettings/a-side.flag) == "1" ] && [ $(cat $IMPSettings/b-side.flag) == "1" ]; then
+	cp ~/RetroPie/retropiemenu/icons/impstartalla1b1.png ~/RetroPie/retropiemenu/icons/impstartall.png
+fi
+
+tput reset
+exit 0
