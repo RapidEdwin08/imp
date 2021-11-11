9f53f2aea20e52df7ef036fd462064335ee5277f
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2021-11-11 14:50:45 -0500

    Update Start All Music.sh

--- main-imp/retropiemenu/Start All Music.sh
+++ main-imp/retropiemenu/Start All Music.sh
@@ -24,17 +24,37 @@ cat /dev/null > $IMPPlaylist/init
 cat /dev/null > $IMPPlaylist/abc
 cat /dev/null > $IMPPlaylist/shuffle
 
-# Put something in [BGMdir] If No MP3s found
-mp3BGMa=$(find $BGMa -iname *.mp3 )
-mp3BGMb=$(find $BGMb -iname *.mp3 )
-if [[ "$mp3BGMa" == '' ]]; then cp ~/RetroPie/retropiemenu/icons/impstartbgmm0a.png "$musicDIR/bgm/A-SIDE/e1m2.mp3" > /dev/null 2>&1; fi
-if [[ "$mp3BGMb" == '' ]]; then cp ~/RetroPie/retropiemenu/icons/impstartbgmm0b.png "$musicDIR/bgm/B-SIDE/ddtblu.mp3" > /dev/null 2>&1; fi
+# Put something in [musicDIR] If No MP3s found at all
+mp3MUSIC=$(find $musicDIR -iname *.mp3 )
+if [[ "$mp3MUSIC" == '' ]]; then cp ~/RetroPie/retropiemenu/icons/impstartallm0.png "$musicDIR/CCCool.mp3" > /dev/null 2>&1; fi
 
-# Add all MP3s from BGM A-SIDE directory to Playlist Non-Recursive
-find "$BGMa" -maxdepth 1 -type f -name "*.mp3" > $IMPPlaylist/abc
+# If BGMa flag 1 - Put something in [BGMadir] If No MP3s found
+if [ "$(cat $IMPSettings/a-side.flag)" == '1' ]; then
+	mp3BGMa=$(find $BGMa -iname *.mp3 )
+	if [[ "$mp3BGMa" == '' ]]; then cp ~/RetroPie/retropiemenu/icons/impstartbgmm0a.png "$musicDIR/bgm/A-SIDE/e1m2.mp3" > /dev/null 2>&1; fi
+fi
 
-# Add all MP3s from BGM B-SIDE directory to Playlist Non-Recursive
-find "$BGMb" -maxdepth 1 -type f -name "*.mp3" > $IMPPlaylist/shuffle
+# If BGMb flag 1 - Put something in [BGMbdir] If No MP3s found
+if [ "$(cat $IMPSettings/b-side.flag)" == '1' ]; then
+	mp3BGMb=$(find $BGMb -iname *.mp3 )
+	if [[ "$mp3BGMb" == '' ]]; then cp ~/RetroPie/retropiemenu/icons/impstartbgmm0b.png "$musicDIR/bgm/B-SIDE/ddtblu.mp3" > /dev/null 2>&1; fi
+fi
+
+# Add all MP3s from musicROMS directory to Playlist Non-Recursive
+find "$musicDIR" -maxdepth 1 -type f -name "*.mp3" > $IMPPlaylist/abc
+
+# Add all MP3s from musicROMS SUB-directories to Playlist Recursive
+find "$musicDIR"/*/ -iname "*.mp3" > $IMPPlaylist/shuffle
+
+# Remove BGM A-SIDE/B-SIDE MP3s from musicROMS SUB-directories According to BGM Settings
+if [ "$(cat $IMPSettings/a-side.flag)" == '0' ]; then
+	cat "$IMPPlaylist/shuffle" | grep -Fv 'imp/music/bgm/A-SIDE/' > $IMPPlaylist/init
+	cat "$IMPPlaylist/init" > $IMPPlaylist/shuffle
+fi
+if [ "$(cat $IMPSettings/b-side.flag)" == '0' ]; then
+	cat "$IMPPlaylist/shuffle" | grep -Fv 'imp/music/bgm/B-SIDE/' > $IMPPlaylist/init
+	cat "$IMPPlaylist/init" > $IMPPlaylist/shuffle
+fi
 
 # Sort INIT playlist with desired 0rder
 cat $IMPPlaylist/abc | sort -n > $IMPPlaylist/init
@@ -43,7 +63,7 @@ cat $IMPPlaylist/shuffle | sort -n >> $IMPPlaylist/init
 # Rebuild ABC and Shuffle Playlists with updated 0rder
 cat $IMPPlaylist/init > $IMPPlaylist/abc
 cat $IMPPlaylist/init | sort --random-sort > $IMPPlaylist/shuffle
-	
+
 # Escape the /\slashes/\ in the Paths
 ESCmusicROMS=${musicROMS//\//\\/}
 ESCmusicDIR=${musicDIR//\//\\/}
@@ -56,7 +76,3 @@ sed -i s+$ESCmusicDIR+$ESCmusicROMS+ $IMPPlaylist/init
 # Start the Music Player Loop Script
 bash "$IMP/play.sh"
 exit 0
-
-# Add all MP3s from BGM A-SIDE + B-SIDE directory to Playlist Non-Recursive
-find "$BGMa" -maxdepth 1 -type f -name "*.mp3" > $IMPPlaylist/init
-find "$BGMb" -maxdepth 1 -type f -name "*.mp3" >> $IMPPlaylist/init
