682345954001fcd667927516938257b10d7698e7
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2021-11-12 00:28:32 -0500

    Update Next Track.sh

--- main-imp/retropiemenu/Next Track.sh
+++ main-imp/retropiemenu/Next Track.sh
@@ -3,8 +3,11 @@ IMP=/opt/retropie/configs/imp
 IMPSettings=$IMP/settings
 IMPPlaylist=$IMP/playlist
 # FULL MODE Write to Disk - LITE MODE Write to tmpfs - Recall Last Track/Position Lost on REBOOT using LITE MODE
-currentTRACK=$IMPPlaylist/current-track
-if [ $(cat $IMPSettings/lite.flag) == "1" ]; then currentTRACK=/dev/shm/current-track; fi
+if [ $(cat $IMPSettings/lite.flag) == "0" ]; then
+	currentTRACK=$IMPPlaylist/current-track
+else
+	currentTRACK=/dev/shm/current-track
+fi
 
 if [[ $(cat $IMPSettings/music-switch.flag) == "1" || $(cat $IMPSettings/pause.flag) == "1" ]]; then
 	pkill -STOP mpg123  > /dev/null 2>&1
