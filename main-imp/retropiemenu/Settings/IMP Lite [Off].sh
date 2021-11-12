b7c8f6b1fd2fb0faab658e3f555c02b3d1fc5d6f
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2021-11-11 23:52:35 -0500

    Add files via upload

--- main-imp/retropiemenu/Settings/IMP Lite [Off].sh
+++ main-imp/retropiemenu/Settings/IMP Lite [Off].sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 IMP=/opt/retropie/configs/imp
 IMPSettings=$IMP/settings
+IMPPlaylist=$IMP/playlist
 
 if [[ $(cat $IMPSettings/lite.flag) == "1" ]]; then
-	# Turning LITE MODE Off
-	echo "0" > $IMPSettings/lite.flag
-	
-	# Swap Icons of Limited Features in LITE Mode
-	rm ~/RetroPie/retropiemenu/icons/impprevious.png
-	cp ~/RetroPie/retropiemenu/icons/impprevious0n.png ~/RetroPie/retropiemenu/icons/impprevious.png
-	
 	if [[ $(cat $IMPSettings/music-switch.flag) == "1" ]]; then
 		# Stop mpg123loop
-		bash "$IMP/stop.sh"> /dev/null 2>&1
+		bash "$IMP/stop.sh" continue > /dev/null 2>&1
+		
+		# Carry 0ver Current Track Contents to [Disk] for FULL MODE
+		cat /dev/shm/current-track > $IMPPlaylist/current-track
+		rm /dev/shm/current-track > /dev/null 2>&1
 	
+		# Turning LITE MODE Off
+		echo "0" > $IMPSettings/lite.flag
+		
 		# Resume Playback
 		bash "$IMP/play.sh" &
 		exit 0
@@ -21,8 +22,15 @@ if [[ $(cat $IMPSettings/lite.flag) == "1" ]]; then
 	
 	if [[ ! $(cat $IMPSettings/pause.flag) == "0" ]]; then
 		# Stop mpg123loop
-		bash "$IMP/stop.sh"> /dev/null 2>&1
+		bash "$IMP/stop.sh" continue > /dev/null 2>&1
 	fi
+	
+	# Carry 0ver Current Track Contents to [Disk] for FULL MODE
+	cat /dev/shm/current-track > $IMPPlaylist/current-track
+	rm /dev/shm/current-track > /dev/null 2>&1
+	
+	# Turning LITE MODE Off
+	echo "0" > $IMPSettings/lite.flag
 fi
 
 tput reset
--- main-imp/retropiemenu/Settings/IMP Lite [On].sh
+++ main-imp/retropiemenu/Settings/IMP Lite [On].sh
@@ -4,21 +4,17 @@ IMPSettings=$IMP/settings
 IMPPlaylist=$IMP/playlist
 
 if [[ $(cat $IMPSettings/lite.flag) == "0" ]]; then
-	# Turning LITE MODE On
-	echo "1" > $IMPSettings/lite.flag
-	
-	# Swap Icons of Limited Features in LITE Mode
-	rm ~/RetroPie/retropiemenu/icons/impprevious.png
-	cp ~/RetroPie/retropiemenu/icons/impprevious0ff.png ~/RetroPie/retropiemenu/icons/impprevious.png
-	
 	if [[ $(cat $IMPSettings/music-switch.flag) == "1" ]]; then
 		# Stop mpg123loop
-		bash "$IMP/stop.sh"> /dev/null 2>&1
+		bash "$IMP/stop.sh" continue > /dev/null 2>&1
 		
-		# Clear Current Track Contents for LITE MODE - Set Last Position 0000+0000
-		echo "" > $IMPPlaylist/current-track
-		echo -e '> 0000+0000  00:00.' >> $IMPPlaylist/current-track
+		# Carry 0ver Current Track Contents to [tmpfs] for LITE MODE
+		cat $IMPPlaylist/current-track > /dev/shm/current-track
+		rm $IMPPlaylist/current-track > /dev/null 2>&1
 	
+		# Turning LITE MODE On
+		echo "1" > $IMPSettings/lite.flag
+		
 		# Resume Playback
 		bash "$IMP/play.sh" &
 		exit 0
@@ -26,12 +22,15 @@ if [[ $(cat $IMPSettings/lite.flag) == "0" ]]; then
 	
 	if [[ ! $(cat $IMPSettings/pause.flag) == "0" ]]; then
 		# Stop mpg123loop
-		bash "$IMP/stop.sh"> /dev/null 2>&1
+		bash "$IMP/stop.sh" continue > /dev/null 2>&1
 	fi
 	
-	# Clear Current Track Contents for LITE MODE - Set Last Position 0000+0000
-	echo "" > $IMPPlaylist/current-track
-	echo -e '> 0000+0000  00:00.' >> $IMPPlaylist/current-track
+	# Carry 0ver Current Track Contents to [tmpfs] for LITE MODE
+	cat $IMPPlaylist/current-track > /dev/shm/current-track
+	rm $IMPPlaylist/current-track > /dev/null 2>&1
+	
+	# Turning LITE MODE On
+	echo "1" > $IMPSettings/lite.flag
 fi
 
 tput reset
