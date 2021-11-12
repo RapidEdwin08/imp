cc7e99602620bfbb8ebd1971eea2ac20cfa93ffa
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2021-11-12 00:27:59 -0500

    Update previous.sh

--- main-imp/configs-imp/previous.sh
+++ main-imp/configs-imp/previous.sh
@@ -10,8 +10,11 @@ IMP=/opt/retropie/configs/imp
 IMPSettings=$IMP/settings
 IMPPlaylist=$IMP/playlist
 # FULL MODE Write to Disk - LITE MODE Write to tmpfs - Recall Last Track/Position Lost on REBOOT using LITE MODE
-currentTRACK=$IMPPlaylist/current-track
-if [ $(cat $IMPSettings/lite.flag) == "1" ]; then currentTRACK=/dev/shm/current-track; fi
+if [ $(cat $IMPSettings/lite.flag) == "0" ]; then
+	currentTRACK=$IMPPlaylist/current-track
+else
+	currentTRACK=/dev/shm/current-track
+fi
 
 # Full Stop
 bash $IMP/stop.sh
