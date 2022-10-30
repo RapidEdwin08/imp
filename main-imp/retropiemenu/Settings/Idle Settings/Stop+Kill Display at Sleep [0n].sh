20c84a7859f5b2febb9f9869dd57fad30378454a
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2022-10-30 13:03:03 -0400

    v2022.10

---
+++ main-imp/retropiemenu/Settings/Idle Settings/Stop+Kill Display at Sleep [0n].sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Create ES Scripts Folder
+mkdir /opt/retropie/configs/all/emulationstation/scripts > /dev/null 2>&1
+
+# Enable Idle IMP @ESSleep
+mkdir /opt/retropie/configs/all/emulationstation/scripts/sleep > /dev/null 2>&1
+echo '#!/bin/bash
+bash /opt/retropie/configs/imp/run-onstart.sh sleep &
+exit 0' > /opt/retropie/configs/all/emulationstation/scripts/sleep/impstop.sh
+sudo chmod 755 /opt/retropie/configs/all/emulationstation/scripts/sleep/impstop.sh
+
+mkdir /opt/retropie/configs/all/emulationstation/scripts/wake > /dev/null 2>&1
+echo '#!/bin/bash
+bash /opt/retropie/configs/imp/run-onend.sh sleep &
+exit 0' > /opt/retropie/configs/all/emulationstation/scripts/wake/impstart.sh
+sudo chmod 755 /opt/retropie/configs/all/emulationstation/scripts/wake/impstart.sh
+
+# IMP Kill Display @ESSleep
+cat /opt/retropie/configs/imp/impXdisplay0.sh > /opt/retropie/configs/all/emulationstation/scripts/sleep/impXdisplay0.sh
+sudo chmod 755 /opt/retropie/configs/all/emulationstation/scripts/sleep/impXdisplay0.sh
+
+cat /opt/retropie/configs/imp/impXdisplay1.sh > /opt/retropie/configs/all/emulationstation/scripts/wake/impXdisplay1.sh
+sudo chmod 755 /opt/retropie/configs/all/emulationstation/scripts/wake/impXdisplay1.sh
+
+#tput reset
+exit 0
