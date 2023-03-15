7c7180f99275f9e3f3ab2399de801cea3742ade9
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2023-03-15 19:31:22 -0400

    LITE Mode Retain Track Position at Poweroff

--- main-imp/configs-imp/impquit.sh
+++ main-imp/configs-imp/impquit.sh
@@ -2,7 +2,14 @@
 # For use with /etc/systemd/system/impquit.service
 
 # Stop IMP - May not be required but still added here anyway
-bash /opt/retropie/configs/imp/stop.sh > /dev/null 2>&1
+bash /opt/retropie/configs/imp/stop.sh continue > /dev/null 2>&1
+
+# Retain Current Track Position in LITE Mode - Grab from TMPFS and Move to DISK If Found - IMP boot.sh will Move Back to TMPFS
+if [ -f /dev/shm/current-track ] && [ $(cat /opt/retropie/configs/imp/settings/lite.flag) == "1" ]; then
+	# Don't Move the 0riginal [current-track] - This might be Called From ES Quit Scripts / PC Utilites
+	cp /dev/shm/current-track /dev/shm/current-track.lite
+	mv /dev/shm/current-track.lite /opt/retropie/configs/imp/playlist/current-track > /dev/null 2>&1
+fi
 
 # Do not play the quitsong if it's already found @ES Quit
 if [ -f /opt/retropie/configs/all/emulationstation/scripts/quit/quitsong.sh ]; then exit 0; fi
