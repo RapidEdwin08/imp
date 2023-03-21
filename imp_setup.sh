71a2c94d91e0265f2e484420c47ac0da2cb988e9
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2023-03-21 18:25:11 -0400

    Update imp_setup.sh

--- imp_setup.sh
+++ imp_setup.sh
@@ -1378,12 +1378,12 @@ sudo chmod 755 /opt/retropie/configs/all/emulationstation/scripts/screensaver-st
 # Enable Idle IMP @ESSleep
 mkdir /opt/retropie/configs/all/emulationstation/scripts/sleep > /dev/null 2>&1
 echo '#!/bin/bash
-bash /opt/retropie/configs/imp/run-onstart.sh idle &
+bash /opt/retropie/configs/imp/run-onstart.sh sleep &
 exit 0' > /opt/retropie/configs/all/emulationstation/scripts/sleep/impstop.sh
 sudo chmod 755 /opt/retropie/configs/all/emulationstation/scripts/sleep/impstop.sh
 mkdir /opt/retropie/configs/all/emulationstation/scripts/wake > /dev/null 2>&1
 echo '#!/bin/bash
-bash /opt/retropie/configs/imp/run-onend.sh idle &
+bash /opt/retropie/configs/imp/run-onend.sh sleep &
 exit 0' > /opt/retropie/configs/all/emulationstation/scripts/wake/impstart.sh
 sudo chmod 755 /opt/retropie/configs/all/emulationstation/scripts/wake/impstart.sh
 rm /opt/retropie/configs/all/emulationstation/scripts/sleep/impXdisplay0.sh > /dev/null 2>&1
