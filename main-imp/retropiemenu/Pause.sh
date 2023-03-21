4ef493882301a085b1465d35ae3decddc39e57b4
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2023-03-21 15:51:35 -0400

    Adjust OMXM0N for Paused state

--- main-imp/retropiemenu/Pause.sh
+++ main-imp/retropiemenu/Pause.sh
@@ -8,6 +8,14 @@ echo "1" > $IMPSettings/pause.flag
 # Set [music-switch.flag]
 echo "0" > $IMPSettings/music-switch.flag
 
+# Stop 0mxmon while paused
+if [ $(cat /opt/retropie/configs/imp/settings/0mxmon.flag) == "1" ]; then
+	rm /dev/shm/0mxMonLoop.Active > /dev/null 2>&1
+	# kill instances of 0mxmon script
+	PIDplayloop=$(ps -eaf | grep "0mxmon.sh" | awk '{print $2}')
+	kill $PIDplayloop > /dev/null 2>&1
+fi
+
 # Pause mpg123
 pkill -STOP mpg123 > /dev/null 2>&1
 
