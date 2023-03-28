b306e2be6b95d2a634bb5877a8a1395e387de17c
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2023-03-27 23:12:41 -0400

    Create run-onbashrc.sh

---
+++ main-imp/configs-imp/run-onbashrc.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+IMP=/opt/retropie/configs/imp
+IMPSettings=$IMP/settings
+
+# A similar version of [run-onstart.sh] just for .bashrc to Respect Fade and kill 0MXM0N
+
+# Stop 0mxmon
+if [ $(cat $IMPSettings/0mxmon.flag) == "1" ]; then
+	rm /dev/shm/0mxMonLoop.Active > /dev/null 2>&1
+	# kill instances of 0mxmon script
+	PIDplayloop=$(ps -eaf | grep "0mxmon.sh" | awk '{print $2}')
+	kill $PIDplayloop > /dev/null 2>&1
+	rm /dev/shm/0mxwaitstart.sh > /dev/null 2>&1
+fi
+
+echo "0" > $IMPSettings/music-switch.flag
+
+# Fade setting determines action
+if [[ $(cat $IMPSettings/fade-out.flag) == "0" ]]; then
+	bash "$IMP/stop.sh" continue > /dev/null 2>&1 #pkill -STOP mpg123
+	exit 0
+fi
+
+# Identify current Audio Device by Pulling AudioDevice Setting from [es_settings.cfg] - Expecting HDMI - PCM - Master - Headphone - Speaker - Digital - Analogue
+audioDEVICEcurrent=$(cat /opt/retropie/configs/all/emulationstation/es_settings.cfg | grep "AudioDevice" | awk -F'=' '{print $3}'| cut -c 2- | rev | cut -c 5- | rev)
+
+# The AudioDevice from [es_settings.cfg] may NOT be the AudioDevice identified by [amixer]
+if [[ ! $(amixer | grep "Simple mixer control" | grep -v 'Capture' ) == *"$audioDEVICEcurrent"* ]]; then 
+	# Identify current Audio Output Device by Process of Elimination - Expecting HDMI - PCM - Master - Headphone - Speaker - Digital - Analogue
+	if [[ $(amixer | grep "Simple mixer control" | grep -v 'Capture' ) == *"HDMI"* ]]; then audioDEVICEcurrent="HDMI"; fi
+	if [[ $(amixer | grep "Simple mixer control" | grep -v 'Capture' ) == *"PCM"* ]]; then audioDEVICEcurrent="PCM"; fi
+	if [[ $(amixer | grep "Simple mixer control" | grep -v 'Capture' ) == *"Master"* ]]; then audioDEVICEcurrent="Master"; fi
+	if [[ $(amixer | grep "Simple mixer control" | grep -v 'Capture' ) == *"Headphone"* ]]; then audioDEVICEcurrent="Headphone"; fi
+	if [[ $(amixer | grep "Simple mixer control" | grep -v 'Capture' ) == *"Speaker"* ]]; then audioDEVICEcurrent="Speaker"; fi
+	if [[ $(amixer | grep "Simple mixer control" | grep -v 'Capture' ) == *"Digital"* ]]; then audioDEVICEcurrent="Digital"; fi
+	if [[ $(amixer | grep "Simple mixer control" | grep -v 'Capture' ) == *"Analogue"* ]]; then audioDEVICEcurrent="Analogue"; fi
+fi
+
+# Parse amixer output for volume - /dB/ or /Left:/ depending on Version of the OS
+# awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master)
+# awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master)
+
+# System may be using pulseaudio - use variable amixerCMD
+amixerCMD=amixer
+
+# Obtain current Volume Setting before fade out - Try BOTH  '/dB/  and   '/Left:/
+currentVOL=$($amixerCMD sget $audioDEVICEcurrent 2>/dev/null | awk -F"[][]" '/dB/ { print $2 }' | cut -d '%' -f 1 )
+if [[ $currentVOL == '' ]]; then
+	currentVOL=$($amixerCMD sget $audioDEVICEcurrent 2>/dev/null | awk -F"[][]" '/Left:/ { print $2 }' | cut -d '%' -f 1 )
+fi
+
+# Try pulseaudio IF STILL Result Expected if ERROR - amixer: Unable to find simple control 
+if [[ $currentVOL == '' ]]; then
+	amixerCMD='amixer -D pulse'
+	# Obtain current Volume Setting before fade out - Try BOTH  '/dB/  and   '/Left:/ AGAIN with [amixer -D pulse]
+	currentVOL=$($amixerCMD sget $audioDEVICEcurrent 2>/dev/null | awk -F"[][]" '/dB/ { print $2 }' | cut -d '%' -f 1 )
+	if [[ $currentVOL == '' ]]; then
+		currentVOL=$($amixerCMD sget $audioDEVICEcurrent 2>/dev/null | awk -F"[][]" '/Left:/ { print $2 }' | cut -d '%' -f 1 )
+	fi
+fi
+
+# Result Expected if ERROR - amixer: Unable to find simple control 
+if [[ $currentVOL == '' ]]; then
+		echo "Unable to Identify Obtain Current VOLUME. Skipping Fade Out..."
+		bash "$IMP/stop.sh" continue > /dev/null 2>&1 #pkill -STOP mpg123
+		exit 0
+fi
+
+# If the last value is anything other than blank then the Volume Fade In [run-onend.sh] Script is Still Running
+if [[ ! "$(cat $IMPSettings/volume.last 2>/dev/null)" == '' ]]; then
+	# kill instances of run-onend.sh scripts - prevents tug of war with Volume Fade In/Out
+	PIDonEND=$(ps -eaf | grep "run-onend.sh" | awk '{print $2}')
+	kill $PIDonEND > /dev/null 2>&1
+	
+	currentVOL=$(cat $IMPSettings/volume.last)
+	$amixerCMD --quiet set "$audioDEVICEcurrent" "$currentVOL"%
+fi
+
+# Set [volume.last]
+echo $currentVOL > $IMPSettings/volume.last
+
+# set Dynamic Volume to check while decreasing volume - '/dB/
+dynamicVOL=$currentVOL
+
+# Decrease Volume until Dynamic Volume reaches 0
+while [ $dynamicVOL -gt 0 ]; do
+	# Decrease Alsa Volume # $amixerCMD -q -c 0 sset "$audioDEVICEcurrent" 1db- unmet no cap # NOT Working with Headphone
+	# High/Med/Low Fade Settings: Workaround for Slow Fade on Certain Builds. eg. PlayBox Vanilla V2
+	if [[ $(cat $IMPSettings/fade-out.flag) == "10" ]]; then #High
+		((dynamicVOL=dynamicVOL-10))
+	elif [[ $(cat $IMPSettings/fade-out.flag) == "5" ]]; then #Medium
+		((dynamicVOL=dynamicVOL-5))
+	else
+		((dynamicVOL=dynamicVOL-1)) #Low
+	fi
+	if [ $dynamicVOL -lt 0 ]; then dynamicVOL=0; fi # amixer Fails with Negative Numbers
+	$amixerCMD --quiet set "$audioDEVICEcurrent" "$dynamicVOL"%
+	if [[ $(cat $IMPSettings/fade-out.flag) == "1" ]]; then sleep 0.025; fi
+done
+
+# Stop/Pause player after Volume 0 - allow time for mpg123 to stop
+bash "$IMP/stop.sh" continue > /dev/null 2>&1 #pkill -STOP mpg123
+sleep 0.2
+
+# set Volume back to Original setting after Fade
+$amixerCMD --quiet set $audioDEVICEcurrent $currentVOL%
+
+exit 0
