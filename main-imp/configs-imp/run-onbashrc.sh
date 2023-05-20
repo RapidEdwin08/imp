6aff38c84b8d5a23e2bafc1141364ea649b84b88
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2023-05-19 23:30:50 -0400

    Update run-onbashrc.sh
    Update FADE for Q4OS

--- main-imp/configs-imp/run-onbashrc.sh
+++ main-imp/configs-imp/run-onbashrc.sh
@@ -39,11 +39,12 @@ fi
 # Parse amixer output for volume - /dB/ or /Left:/ depending on Version of the OS
 # awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master)
 # awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master)
+# awk 'END{print}' | cut -d '%' -f 1 | cut -d[ -f2-
 
 # System may be using pulseaudio - use variable amixerCMD
 amixerCMD=amixer
 
-# Obtain current Volume Setting before fade out - Try BOTH  '/dB/  and   '/Left:/
+# Obtain current Volume Setting before fade - Try BOTH  '/dB/  and   '/Left:/
 currentVOL=$($amixerCMD sget $audioDEVICEcurrent 2>/dev/null | awk -F"[][]" '/dB/ { print $2 }' | cut -d '%' -f 1 )
 if [[ $currentVOL == '' ]]; then
 	currentVOL=$($amixerCMD sget $audioDEVICEcurrent 2>/dev/null | awk -F"[][]" '/Left:/ { print $2 }' | cut -d '%' -f 1 )
@@ -52,18 +53,31 @@ fi
 # Try pulseaudio IF STILL Result Expected if ERROR - amixer: Unable to find simple control 
 if [[ $currentVOL == '' ]]; then
 	amixerCMD='amixer -D pulse'
-	# Obtain current Volume Setting before fade out - Try BOTH  '/dB/  and   '/Left:/ AGAIN with [amixer -D pulse]
+	# Obtain current Volume Setting before fade - Try BOTH  '/dB/  and   '/Left:/ AGAIN with [amixer -D pulse]
 	currentVOL=$($amixerCMD sget $audioDEVICEcurrent 2>/dev/null | awk -F"[][]" '/dB/ { print $2 }' | cut -d '%' -f 1 )
 	if [[ $currentVOL == '' ]]; then
 		currentVOL=$($amixerCMD sget $audioDEVICEcurrent 2>/dev/null | awk -F"[][]" '/Left:/ { print $2 }' | cut -d '%' -f 1 )
 	fi
 fi
 
+# Last Try to Obtain current Volume Setting
+# amixer sget Master 2>/dev/null | awk 'END{print}' | cut -d '%' -f 1 | cut -d[ -f2-
+if [[ $currentVOL == '' ]]; then
+	# Obtain current Volume Setting before fade - Works with Q4OS
+	amixerCMD='amixer'
+	currentVOL=$($amixerCMD sget $audioDEVICEcurrent 2>/dev/null | awk 'END{print}' | cut -d '%' -f 1 | cut -d[ -f2- )
+	if [[ $currentVOL == '' ]]; then
+		amixerCMD='amixer -D pulse'
+		currentVOL=$($amixerCMD sget $audioDEVICEcurrent 2>/dev/null | awk 'END{print}' | cut -d '%' -f 1 | cut -d[ -f2- )
+	fi
+fi
+
 # Result Expected if ERROR - amixer: Unable to find simple control 
 if [[ $currentVOL == '' ]]; then
-		echo "Unable to Identify Obtain Current VOLUME. Skipping Fade Out..."
-		bash "$IMP/stop.sh" continue > /dev/null 2>&1 #pkill -STOP mpg123
-		exit 0
+	#echo "Unable to Identify Obtain Current VOLUME. Skipping Fade ..."
+	echo "2" > $IMPSettings/pause.flag #stop.sh Detects pause.flag 2
+	bash "$IMP/stop.sh" continue > /dev/null 2>&1 #pkill -STOP mpg123
+	exit 0
 fi
 
 # If the last value is anything other than blank then the Volume Fade In [run-onend.sh] Script is Still Running
