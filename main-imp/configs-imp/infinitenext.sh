633575454cc3d22b061e1fa4647c23a310c015d5
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-01-23 06:43:03 -0500

    Update infinitenext.sh

--- main-imp/configs-imp/infinitenext.sh
+++ main-imp/configs-imp/infinitenext.sh
@@ -8,39 +8,6 @@ BGMdir="$musicDIR/bgm"
 BGMa="$musicDIR/bgm/A-SIDE"
 BGMb="$musicDIR/bgm/B-SIDE"
 
-# Stop mpg123loop with continue parameter
-bash "$IMP/stop.sh" continue > /dev/null 2>&1
-
-# Clear playlists
-cat /dev/null > $IMPPlaylist/init
-cat /dev/null > $IMPPlaylist/abc
-cat /dev/null > $IMPPlaylist/shuffle
-
-# Add all MP3s from musicDIR directory to Playlist Non-Recursive
-find "$musicDIR" -maxdepth 1 -type f -iname "*.mp3" > $IMPPlaylist/abc
-
-# Add all MP3s from musicDIR SUB-directories to Playlist Recursive
-find "$musicDIR"/*/ -iname "*.mp3" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' > $IMPPlaylist/shuffle
-
-# Remove BGM A-SIDE/B-SIDE MP3s from musicDIR SUB-directories According to BGM Settings
-if [ "$(cat $IMPSettings/a-side.flag)" == '0' ]; then
-	cat "$IMPPlaylist/shuffle" | grep -Fv 'imp/music/bgm/A-SIDE/' > $IMPPlaylist/init
-	cat "$IMPPlaylist/init" > $IMPPlaylist/shuffle
-fi
-if [ "$(cat $IMPSettings/b-side.flag)" == '0' ]; then
-	cat "$IMPPlaylist/shuffle" | grep -Fv 'imp/music/bgm/B-SIDE/' > $IMPPlaylist/init
-	cat "$IMPPlaylist/init" > $IMPPlaylist/shuffle
-fi
-
-# Sort INIT playlist with desired 0rder
-cat $IMPPlaylist/abc | sort -n > $IMPPlaylist/init
-cat $IMPPlaylist/shuffle | sort -n >> $IMPPlaylist/init
-
-# Rebuild ABC and Shuffle Playlists with updated 0rder
-cat $IMPPlaylist/init > $IMPPlaylist/abc
-cat $IMPPlaylist/init | sort --random-sort > $IMPPlaylist/shuffle
-
-###################
 # FULL MODE Write to Disk - LITE MODE Write to tmpfs - Recall Last Track/Position Lost on REBOOT using LITE MODE
 if [ $(cat $IMPSettings/lite.flag) == "0" ]; then
 	currentTRACK=$IMPPlaylist/current-track
@@ -61,8 +28,47 @@ dirBASE=$(grep -iE 'Directory:' $currentTRACK | cut -b 12-999)
 # Expected Result stream http://ice1.somafm.com/lush-128-mp3
 mp3BASE=$dirBASE$mp3LAST
 
+# Stop mpg123loop with continue parameter
+bash "$IMP/stop.sh" continue > /dev/null 2>&1
+
+# Clear playlists
+cat /dev/null > $IMPPlaylist/init
+cat /dev/null > $IMPPlaylist/abc
+cat /dev/null > $IMPPlaylist/shuffle
+
+###################
+
+# Add all MP3s from musicDIR directory to Playlist Non-Recursive
+find "$musicDIR" -maxdepth 1 -type f -iname "*.mp3" > $IMPPlaylist/abc
+
+# Add all MP3s from musicDIR SUB-directories to Playlist Recursive
+find "$musicDIR"/*/ -iname "*.mp3" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' > $IMPPlaylist/shuffle
+
+# Remove BGM A-SIDE MP3s from musicDIR SUB-directories According to BGM Settings
+if [ ! "$dirBASE" == "$BGMdir/"* 2>/dev/null ] && [ "$(cat $IMPSettings/a-side.flag)" == '0' ]; then
+	cat "$IMPPlaylist/shuffle" | grep -v 'imp/music/bgm/A-SIDE/' > $IMPPlaylist/init
+	cat "$IMPPlaylist/init" > $IMPPlaylist/shuffle
+fi
+# Remove BGM B-SIDE MP3s from musicDIR SUB-directories According to BGM Settings
+# Skip if currentTRACK is in BGM A-SIDE to Continue to B-SIDE
+if [ ! "$dirBASE" == "$BGMdir/"* 2>/dev/null ] && [ "$(cat $IMPSettings/b-side.flag)" == '0' ]; then
+	cat "$IMPPlaylist/shuffle" | grep -v 'imp/music/bgm/B-SIDE/' > $IMPPlaylist/init
+	cat "$IMPPlaylist/init" > $IMPPlaylist/shuffle
+fi
+
+# Sort INIT playlist with desired 0rder
+cat $IMPPlaylist/abc | sort -n > $IMPPlaylist/init
+cat $IMPPlaylist/shuffle | sort -n >> $IMPPlaylist/init
+
+# Rebuild ABC and Shuffle Playlists with updated 0rder
+cat $IMPPlaylist/init > $IMPPlaylist/abc
+cat $IMPPlaylist/init | sort --random-sort > $IMPPlaylist/shuffle
+
+###################
+
 # Determine ABC or Shuffle Playlist from Settings Flags
 if [ $(cat $IMPSettings/shuffle.flag) == "1" ]; then
+#if [ ! $(cat $IMPSettings/randomizerboot.flag) == "0" ]; then
 	currentPLIST="$IMPPlaylist/shuffle"
 else
 	currentPLIST="$IMPPlaylist/abc"
@@ -74,21 +80,23 @@ if [[ "$mp3BASE" == '' ]]; then mp3BASE=$(head -qn1 $currentPLIST); fi
 # Determine # of Lines - Shortens the amount of time it takes to Parse Lines with grep
 LINEcount=$(grep -c ".*" $currentPLIST)
 
-# Parse all lines after and before Last Track played
+# Parse all lines after and before Last Track played - FIlter out anything in the same Directory
 grep -FA $LINEcount "$mp3LAST" $currentPLIST | grep -v "$mp3LAST" | grep -v "$dirBASE" > $IMPPlaylist/init
 grep -FB $LINEcount "$mp3LAST" $currentPLIST | grep -v "$mp3LAST" | grep -v "$dirBASE" >> $IMPPlaylist/init
 
-# Parse all lines after and before Last Track played again if nothing
+# Parse all lines after and before Last Track played again if nothing - Alternate FIlter
 if [[ "$(cat $IMPPlaylist/init)" == '' ]]; then
 	grep -FA $LINEcount "$mp3BASE" $currentPLIST | grep -Fv "$mp3BASE" > $IMPPlaylist/init
 	grep -FB $LINEcount "$mp3BASE" $currentPLIST | grep -Fv "$mp3BASE" >> $IMPPlaylist/init
 fi
 
 ###################
+# nextTRACK obtained from filtered down list
 nextTRACK=$(head -qn1 $IMPPlaylist/init)
+#echo $nextTRACK
 
 # Run Randomizer if still nothing
-if [[ "$nextTRACK" == '' ]]; then bash "$IMP/randomizer.sh" & exit 0; fi
+if [ "$nextTRACK" == '' ]; then bash "$IMP/randomizer.sh" & exit 0; fi
 
 # Start the Music Player Loop Script
 bash "$IMP/rom.sh" "$nextTRACK" &
