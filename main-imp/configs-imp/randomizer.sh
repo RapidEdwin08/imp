59318b0cb30c0b883b581ed10ba8d7b03cabbc5f
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-01-25 22:16:47 -0500

    v2025.01

--- main-imp/configs-imp/randomizer.sh
+++ main-imp/configs-imp/randomizer.sh
@@ -13,41 +13,41 @@ allMUSIC()
 # BGM Flags AB
 if [ $(cat $IMPSettings/a-side.flag) == "1" ] && [ $(cat $IMPSettings/b-side.flag) == "1" ]; then
 	# Find Random [.mp3] - ALL+BGM_AB
-	find "$musicDIR"/* -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf > /dev/shm/mp3.tmp
-	find "$musicDIR"/*/ -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf >> /dev/shm/mp3.tmp
-	cat /dev/shm/mp3.tmp | sort --random-sort > /dev/shm/pls.tmp
-	randomMP3=$(cat /dev/shm/pls.tmp | shuf | tail -n 1)
-	rm /dev/shm/mp3.tmp; rm /dev/shm/pls.tmp
+	find "$musicDIR" -maxdepth 1 -type f -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf > /dev/shm/init
+	find "$musicDIR"/*/ -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf >> /dev/shm/init
+	cat /dev/shm/init | sort --random-sort > /dev/shm/pls
+	randomMP3=$(cat /dev/shm/pls | shuf | tail -n 1)
+	rm /dev/shm/init; rm /dev/shm/pls
 fi
 
 # BGM Flags A
 if [ $(cat $IMPSettings/a-side.flag) == "1" ] && [ $(cat $IMPSettings/b-side.flag) == "0" ]; then
 	# Find Random [.mp3] - ALL+BGM_A
-	find "$musicDIR"/* -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v "/bgm/B-SIDE/" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf > /dev/shm/mp3.tmp
-	find "$musicDIR"/*/ -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v "/bgm/B-SIDE/" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf >> /dev/shm/mp3.tmp
-	cat /dev/shm/mp3.tmp | sort --random-sort > /dev/shm/pls.tmp
-	randomMP3=$(cat /dev/shm/pls.tmp | shuf | tail -n 1)
-	rm /dev/shm/mp3.tmp; rm /dev/shm/pls.tmp
+	find "$musicDIR" -maxdepth 1 -type f -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v "/bgm/B-SIDE/" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf > /dev/shm/init
+	find "$musicDIR"/*/ -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v "/bgm/B-SIDE/" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf >> /dev/shm/init
+	cat /dev/shm/init | sort --random-sort > /dev/shm/pls
+	randomMP3=$(cat /dev/shm/pls | shuf | tail -n 1)
+	rm /dev/shm/init; rm /dev/shm/pls
 fi
 
 # BGM Flags B
 if [ $(cat $IMPSettings/a-side.flag) == "0" ] && [ $(cat $IMPSettings/b-side.flag) == "1" ]; then
 	# Find Random [.mp3] - ALL+BGM_B
-	find "$musicDIR"/* -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v "/bgm/A-SIDE/" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf > /dev/shm/mp3.tmp
-	find "$musicDIR"/*/ -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v "/bgm/A-SIDE/" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf >> /dev/shm/mp3.tmp
-	cat /dev/shm/mp3.tmp | sort --random-sort > /dev/shm/pls.tmp
-	randomMP3=$(cat /dev/shm/pls.tmp | shuf | tail -n 1)
-	rm /dev/shm/mp3.tmp; rm /dev/shm/pls.tmp
+	find "$musicDIR" -maxdepth 1 -type f -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v "/bgm/A-SIDE/" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf > /dev/shm/init
+	find "$musicDIR"/*/ -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v "/bgm/A-SIDE/" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf >> /dev/shm/init
+	cat /dev/shm/init | sort --random-sort > /dev/shm/pls
+	randomMP3=$(cat /dev/shm/pls | shuf | tail -n 1)
+	rm /dev/shm/init; rm /dev/shm/pls
 fi
 
 # BGM Flags ' '
 if [ $(cat $IMPSettings/a-side.flag) == "0" ] && [ $(cat $IMPSettings/b-side.flag) == "0" ]; then
 	# Find Random [.mp3] - ALL (NO BGM)
-	find "$musicDIR"/* -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v "/bgm/A-SIDE/" | grep -v "/bgm/B-SIDE/" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf > /dev/shm/mp3.tmp
-	find "$musicDIR"/*/ -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v "/bgm/A-SIDE/" | grep -v "/bgm/B-SIDE/" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf >> /dev/shm/mp3.tmp
-	cat /dev/shm/mp3.tmp | sort --random-sort > /dev/shm/pls.tmp
-	randomMP3=$(cat /dev/shm/pls.tmp | shuf | tail -n 1)
-	rm /dev/shm/mp3.tmp; rm /dev/shm/pls.tmp
+	find "$musicDIR" -maxdepth 1 -type f -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v "/bgm/A-SIDE/" | grep -v "/bgm/B-SIDE/" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf > /dev/shm/init
+	find "$musicDIR"/*/ -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v "/bgm/A-SIDE/" | grep -v "/bgm/B-SIDE/" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf >> /dev/shm/init
+	cat /dev/shm/init | sort --random-sort > /dev/shm/pls
+	randomMP3=$(cat /dev/shm/pls | shuf | tail -n 1)
+	rm /dev/shm/init; rm /dev/shm/pls
 fi
 }
 
@@ -55,23 +55,22 @@ bgmAB()
 {
 # Find Random [.mp3] - BGM_AB
 #randomMP3=$(find "$BGMdir"/*/ -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf | tail -n 1)
-find "$BGMdir"/* -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf > /dev/shm/mp3.tmp
-find "$BGMdir"/*/ -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf >> /dev/shm/mp3.tmp
-cat /dev/shm/mp3.tmp | sort --random-sort > /dev/shm/pls.tmp
-randomMP3=$(cat /dev/shm/pls.tmp | shuf | tail -n 1)
-rm /dev/shm/mp3.tmp; rm /dev/shm/pls.tmp
+find "$BGMdir"/* -maxdepth 1 -type f -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf > /dev/shm/init
+find "$BGMdir"/*/ -iname "*.mp3" | grep -vi .m3u | grep -vi .pls | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' | grep -i .mp3 | shuf >> /dev/shm/init
+cat /dev/shm/init | sort --random-sort > /dev/shm/pls
+randomMP3=$(cat /dev/shm/pls | shuf | tail -n 1)
+rm /dev/shm/init; rm /dev/shm/pls
 }
 
 m3uPLS()
 {
-# Find Random [.mp3] - BGM_AB
-find "$musicDIR"/* -iname "*.pls" > /dev/shm/mp3.tmp
-find "$musicDIR"/*/ -iname "*.pls" > /dev/shm/mp3.tmp
-find "$musicDIR"/* -iname "*.m3u" >> /dev/shm/mp3.tmp
-find "$musicDIR"/*/ -iname "*.m3u" >> /dev/shm/mp3.tmp
-cat /dev/shm/mp3.tmp | sort --random-sort > /dev/shm/pls.tmp
-randomMP3=$(cat /dev/shm/pls.tmp | shuf | tail -n 1)
-rm /dev/shm/mp3.tmp; rm /dev/shm/pls.tmp
+# Find Random [.pls/.m3u] Files
+find "$musicDIR" -maxdepth 1 -type f -iname "*.pls" > /dev/shm/init
+find "$musicDIR"/*/ -iname "*.pls" > /dev/shm/init
+find "$musicDIR" -maxdepth 1 -type f -iname "*.m3u" >> /dev/shm/init
+find "$musicDIR"/*/ -iname "*.m3u" >> /dev/shm/init
+randomMP3=$(cat /dev/shm/init | shuf | tail -n 1)
+rm /dev/shm/init
 }
 
 # Turning SHUFFLE On *202501 Changed my mind...
