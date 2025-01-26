80f5394cc8c0604a45bfb372b85f900a239c3c23
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-01-25 22:20:28 -0500

    v2025.01

--- main-imp/retropiemenu/Start All Music.sh
+++ main-imp/retropiemenu/Start All Music.sh
@@ -11,34 +11,32 @@ BGMb="$musicDIR/bgm/B-SIDE"
 # Stop mpg123loop with continue parameter
 bash "$IMP/stop.sh" continue > /dev/null 2>&1
 
-# Clear playlists
-cat /dev/null > $IMPPlaylist/init
-cat /dev/null > $IMPPlaylist/abc
-cat /dev/null > $IMPPlaylist/shuffle
-
 # Add all MP3s from musicDIR directory to Playlist Non-Recursive
-find "$musicDIR" -maxdepth 1 -type f -iname "*.mp3" > $IMPPlaylist/abc
+find "$musicDIR" -maxdepth 1 -type f -iname "*.mp3" > /dev/shm/abc
 
 # Add all MP3s from musicDIR SUB-directories to Playlist Recursive
-find "$musicDIR"/*/ -iname "*.mp3" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' > $IMPPlaylist/shuffle
+find "$musicDIR"/*/ -iname "*.mp3" | grep -v 'imp/music/bgm/startup.mp3' | grep -v 'imp/music/bgm/quit.mp3' > /dev/shm/shuffle
 
 # Remove BGM A-SIDE/B-SIDE MP3s from musicDIR SUB-directories According to BGM Settings
 if [ "$(cat $IMPSettings/a-side.flag)" == '0' ]; then
-	cat "$IMPPlaylist/shuffle" | grep -Fv 'imp/music/bgm/A-SIDE/' > $IMPPlaylist/init
-	cat "$IMPPlaylist/init" > $IMPPlaylist/shuffle
+	cat "/dev/shm/shuffle" | grep -Fv 'imp/music/bgm/A-SIDE/' > /dev/shm/init
+	cat "/dev/shm/init" > /dev/shm/shuffle
 fi
 if [ "$(cat $IMPSettings/b-side.flag)" == '0' ]; then
-	cat "$IMPPlaylist/shuffle" | grep -Fv 'imp/music/bgm/B-SIDE/' > $IMPPlaylist/init
-	cat "$IMPPlaylist/init" > $IMPPlaylist/shuffle
+	cat "/dev/shm/shuffle" | grep -Fv 'imp/music/bgm/B-SIDE/' > /dev/shm/init
+	cat "/dev/shm/init" > /dev/shm/shuffle
 fi
 
 # Sort INIT playlist with desired 0rder
-cat $IMPPlaylist/abc | sort -n > $IMPPlaylist/init
-cat $IMPPlaylist/shuffle | sort -n >> $IMPPlaylist/init
+cat /dev/shm/abc | sort -n > $IMPPlaylist/init
+cat /dev/shm/shuffle | sort -n >> $IMPPlaylist/init
 
 # Rebuild ABC and Shuffle Playlists with updated 0rder
 cat $IMPPlaylist/init > $IMPPlaylist/abc
 cat $IMPPlaylist/init | sort --random-sort > $IMPPlaylist/shuffle
+
+# Clean up
+rm /dev/shm/abc 2>/dev/null; rm /dev/shm/shuffle 2>/dev/null; rm /dev/shm/init 2>/dev/null
 
 # Start the Music Player Loop Script
 bash "$IMP/play.sh"
