2c8d4a9ac79f001b72bcde079f9c8aa47a08bf78
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-10-23 08:21:27 -0400

    Update Pause.sh
    Remove 0mxmon reference

--- main-imp/retropiemenu/Pause.sh
+++ main-imp/retropiemenu/Pause.sh
@@ -9,13 +9,13 @@ echo "1" > $IMPSettings/pause.flag
 echo "0" > $IMPSettings/music-switch.flag
 
 # Stop 0mxmon while paused
-if [ $(cat /opt/retropie/configs/imp/settings/0mxmon.flag) == "1" ]; then
-	rm /dev/shm/0mxMonLoop.Active > /dev/null 2>&1
-	rm /dev/shm/0mxwaitstart.sh > /dev/null 2>&1
+#if [ $(cat /opt/retropie/configs/imp/settings/0mxmon.flag) == "1" ]; then
+	#rm /dev/shm/0mxMonLoop.Active > /dev/null 2>&1
+	#rm /dev/shm/0mxwaitstart.sh > /dev/null 2>&1
 	# kill instances of 0mxmon script
-	PIDplayloop=$(ps -eaf | grep "0mxmon.sh" | awk '{print $2}')
-	kill $PIDplayloop > /dev/null 2>&1
-fi
+	#PIDplayloop=$(ps -eaf | grep "0mxmon.sh" | awk '{print $2}')
+	#kill $PIDplayloop > /dev/null 2>&1
+#fi
 
 # Pause mpg123
 pkill -STOP mpg123 > /dev/null 2>&1
