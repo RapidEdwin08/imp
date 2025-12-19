8bc84c9b2dbbce24521792551735d7fb58d9d2c7
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-12-19 14:07:09 -0500

    Add MUTE; Hide Errors for IMP-CLI

--- main-imp/templates/Current Playlist.sh
+++ main-imp/templates/Current Playlist.sh
@@ -7,10 +7,10 @@ musicDIR=~/RetroPie/retropiemenu/imp/music
 # FULL MODE Write to Disk - LITE MODE Write to tmpfs - Recall Last Track/Position Lost on REBOOT using LITE MODE
 if [ $(cat $IMPSettings/lite.flag) == "0" ]; then
 	currentTRACK=$IMPPlaylist/current-track
-	trackTIME=$(grep '>*+' /opt/retropie/configs/imp/playlist/current-track | tail -1 | sed -n -e 's/^.*> //p' |awk '{print $2}' | cut -d '.' -f 1)
+	trackTIME=$(grep '>*+' /opt/retropie/configs/imp/playlist/current-track 2>/dev/null | tail -1 | sed -n -e 's/^.*> //p' |awk '{print $2}' | cut -d '.' -f 1)
 else
 	currentTRACK=/dev/shm/current-track
-	trackTIME=$(grep '>*+' /dev/shm/current-track | tail -1 | sed -n -e 's/^.*> //p' |awk '{print $2}' | cut -d '.' -f 1)
+	trackTIME=$(grep '>*+' /dev/shm/current-track 2>/dev/null | tail -1 | sed -n -e 's/^.*> //p' |awk '{print $2}' | cut -d '.' -f 1)
 fi
 # Startup Song
 if [ -f /dev/shm/startup-track ]; then
@@ -18,32 +18,33 @@ if [ -f /dev/shm/startup-track ]; then
 	trackTIME=$(grep '>*+' /dev/shm/startup-track | tail -1 | sed -n -e 's/^.*> //p' |awk '{print $2}' | cut -d '.' -f 1)
 fi
 
-trackFILE=$(grep -iE 'Playing MPEG stream' $currentTRACK | cut -b 28-999 | perl -ple 'chop' | perl -ple 'chop' | perl -ple 'chop' | perl -ple 'chop')
-trackTITLE=$(grep -iE 'Title:' $currentTRACK); if [ $trackTITLE == 'Title:' 2>/dev/null ]; then trackTITLE=''; fi
-trackALBUM=$(grep -iE 'Album:' $currentTRACK); if [ $trackALBUM == 'Album:' 2>/dev/null ]; then trackALBUM=''; fi
-trackYEAR=$(grep -iE 'Year:' $currentTRACK); if [ $trackYEAR == 'Year:' 2>/dev/null ]; then trackYEAR=''; fi
-trackARTIST=$(grep -iE 'Artist:' $currentTRACK); if [ $trackARTIST == 'Artist:' 2>/dev/null ]; then trackARTIST=''; fi
+trackFILE=$(grep -iE 'Playing MPEG stream' $currentTRACK 2>/dev/null | cut -b 28-999 | perl -ple 'chop' | perl -ple 'chop' | perl -ple 'chop' | perl -ple 'chop')
+trackTITLE=$(grep -iE 'Title:' $currentTRACK 2>/dev/null); if [ $trackTITLE == 'Title:' 2>/dev/null ]; then trackTITLE=''; fi
+trackALBUM=$(grep -iE 'Album:' $currentTRACK 2>/dev/null); if [ $trackALBUM == 'Album:' 2>/dev/null ]; then trackALBUM=''; fi
+trackYEAR=$(grep -iE 'Year:' $currentTRACK 2>/dev/null); if [ $trackYEAR == 'Year:' 2>/dev/null ]; then trackYEAR=''; fi
+trackARTIST=$(grep -iE 'Artist:' $currentTRACK 2>/dev/null); if [ $trackARTIST == 'Artist:' 2>/dev/null ]; then trackARTIST=''; fi
 playerVOL=$(cat $IMPSettings/volume.flag)
-if [ $playerVOL == "32768" ]; then volume_percent="100"; fi
-if [ $playerVOL == "29484" ]; then volume_percent="90"; fi
-if [ $playerVOL == "26208" ]; then volume_percent="80"; fi
-if [ $playerVOL == "22932" ]; then volume_percent="70"; fi
-if [ $playerVOL == "19656" ]; then volume_percent="60"; fi
-if [ $playerVOL == "16380" ]; then volume_percent="50"; fi
-if [ $playerVOL == "13104" ]; then volume_percent="40"; fi
-if [ $playerVOL == "9828" ]; then volume_percent="30"; fi
-if [ $playerVOL == "6552" ]; then volume_percent="20"; fi
-if [ $playerVOL == "3276" ]; then volume_percent="10"; fi
-if [ $playerVOL == "1638" ]; then volume_percent="5"; fi
+if [ $playerVOL == "32768" ]; then volume_percent="%100"; fi
+if [ $playerVOL == "29484" ]; then volume_percent="%90"; fi
+if [ $playerVOL == "26208" ]; then volume_percent="%80"; fi
+if [ $playerVOL == "22932" ]; then volume_percent="%70"; fi
+if [ $playerVOL == "19656" ]; then volume_percent="%60"; fi
+if [ $playerVOL == "16380" ]; then volume_percent="%50"; fi
+if [ $playerVOL == "13104" ]; then volume_percent="%40"; fi
+if [ $playerVOL == "9828" ]; then volume_percent="%30"; fi
+if [ $playerVOL == "6552" ]; then volume_percent="%20"; fi
+if [ $playerVOL == "3276" ]; then volume_percent="%10"; fi
+if [ $playerVOL == "1638" ]; then volume_percent="%5"; fi
+if [ $playerVOL == "0000" ]; then volume_percent="MUTE"; fi
 
 # Parse Current Track file to obtain Last track played - Cut off the last x4 characters
 # Expected Result MP3: Playing MPEG stream 1 of 1: Song.mp3 ...
 # Expected Result Stream: Playing MPEG stream 1 of 1: lush-128-mp3 ...
-mp3LAST=$(grep -iE 'Playing MPEG stream' $currentTRACK | cut -b 29-999 | perl -ple 'chop' | perl -ple 'chop' | perl -ple 'chop' | perl -ple 'chop')
+mp3LAST=$(grep -iE 'Playing MPEG stream' $currentTRACK 2>/dev/null | cut -b 29-999 | perl -ple 'chop' | perl -ple 'chop' | perl -ple 'chop' | perl -ple 'chop')
 
 # Expected Result  Directory: /home/pi/RetroPie/roms/music/
 # Expected Result  Directory: http://ice1.somafm.com/
-dirBASE=$(grep -iE 'Directory:' $currentTRACK | cut -b 12-999)
+dirBASE=$(grep -iE 'Directory:' $currentTRACK 2>/dev/null | cut -b 12-999)
 
 # Expected Result mp3: /home/pi/RetroPie/roms/music/Song.mp3
 # Expected Result stream http://ice1.somafm.com/lush-128-mp3
@@ -52,9 +53,9 @@ mp3BASE=$dirBASE$mp3LAST
 currentBASE=$(echo "$dirBASE" | awk -v FS="retropiemenu/imp/" '{print $2}' )
 if [[ "$currentBASE" == '' ]]; then currentBASE=$(echo "$dirBASE" | awk -v FS="/RetroPie/" '{print $2}' ); fi
 
-streamURL=$(grep -iE 'ICY-URL:' $currentTRACK | cut -b 10-999)
-streamNAME=$(grep -iE 'ICY-NAME:' $currentTRACK | cut -b 11-999)
-streamTITLE=$(grep -iE 'StreamTitle=' $currentTRACK | cut -b 23-999 | cut -d ';' -f 1 )
+streamURL=$(grep -iE 'ICY-URL:' $currentTRACK 2>/dev/null | cut -b 10-999)
+streamNAME=$(grep -iE 'ICY-NAME:' $currentTRACK 2>/dev/null | cut -b 11-999)
+streamTITLE=$(grep -iE 'StreamTitle=' $currentTRACK 2>/dev/null | cut -b 23-999 | cut -d ';' -f 1 )
 pausemode=$(cat $IMPSettings/pause.flag)
 if [[ $pausemode == "1" || $pausemode == "2" ]]; then pausemode="\Z3\ZbPAUSED\Zn"; else pausemode="\Z2PLAYING\Zn"; fi
 
@@ -102,7 +103,7 @@ else
 	plistINFO="\Z0TIME ELAPSED [\Z4$trackTIME\Z0]  SHUFFLE [\Z4$shuffleMODE\Z0]  REPEAT [\Z4$infinite_mode\Z0]"
 fi
 
-currentVOLUME="\Z0Volume [\Z4%$volume_percent\Z0]"
+currentVOLUME="\Z0Volume [\Z4$volume_percent\Z0]"
 
 currentPLIST=$(
 echo
@@ -165,4 +166,4 @@ echo -n "1.."
 sleep 1
 echo -n ".."
 tput reset
-exit 0 
+exit 0
